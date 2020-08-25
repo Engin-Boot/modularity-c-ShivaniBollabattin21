12eea1d40c2d93e0b067f84b1701f8902cd2e725
ShivaniBollabattin21 <68583125+ShivaniBollabattin21@users.noreply.github.com>
2020-08-25 22:33:46 +0530

    Add files via upload

---
+++ TelCo.ColorCoder/PrintManual.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static TelCo.ColorCoder.ColourSpace;
+
+namespace TelCo.ColorCoder
+{
+    class PrintManual
+    {
+        public override string ToString()
+        {
+            string colorCodes = "";
+            for(int i = 1; i < 26; i++)
+            {
+                ColorPair pair = ColorCodingPairNumber.GetColorFromPairNumber(i);
+                colorCodes = colorCodes + "PAIR NUMBER = " + i + " "  + "MAJOR COLOR = " + " " + pair.majorColor + "MINOR COLOR = " +" "+ pair.minorColor + "\n";
+            }
+            return colorCodes;
+        }
+    }
+}
