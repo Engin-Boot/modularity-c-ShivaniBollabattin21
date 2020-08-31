d9cd0f62fee2bcb18fc2edcb1af051887ba145de
ShivaniBollabattin21 <68583125+ShivaniBollabattin21@users.noreply.github.com>
2020-08-31 14:05:29 +0530

    Update ColorCodingColorPair.cs

--- TelCo.ColorCoder/ColorCodingColorPair.cs
+++ TelCo.ColorCoder/ColorCodingColorPair.cs
@@ -6,29 +6,6 @@ namespace TelCo.ColorCoder
 {
     class ColorCodingColorPair
     {
-       
-        public static int GetPairNumberFromColor(ColorPair pair)
-        {
-            int majorIndex = -1;
-            for (int i = 0; i < ColourSpace.getColorMapMajor().Length; i++)
-            {
-                if ((Color)ColourSpace.getColorMapMajor().GetValue(i) == pair.majorColor)
-                {
-                    majorIndex = i;
-                    break;
-                }
-            }
-
-            int minorIndex = -1;
-            for (int i = 0; i < ColourSpace.getColorMapMinor().Length; i++)
-            {using System;
-using static TelCo.ColorCoder.ColourSpace;
-using System.Drawing;
-
-namespace TelCo.ColorCoder
-{
-    class ColorCodingColorPair
-    {
 
         public static int _checkColor(Color[] colorArray, Color _color)
         {
@@ -59,23 +36,5 @@ namespace TelCo.ColorCoder
         }
 
 
-    }
-}
-
-                if ((Color)ColourSpace.getColorMapMinor().GetValue(i) == pair.minorColor)
-                {
-                    minorIndex = i;
-                    break;
-                }
-            }
-            if (majorIndex == -1 || minorIndex == -1)
-            {
-                throw new ArgumentException(
-                    string.Format("Unknown Colors: {0}", pair.ToString()));
-            }
-
-            
-            return (majorIndex * ColourSpace.getColorMapMinor().Length) + (minorIndex + 1);
-        }
     }
 }
